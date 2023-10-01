0400bebaba88ee57322b8136b8d611cb74a847a2
domanskayelyzaveta <domanskaya_@ukr.net>
2023-10-01 20:47:22 +0200

    homework phone correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { ContactsForm } from './ContactsForm/ContactsForm';
 import { Filter } from './Filter/Filter';
-import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -28,19 +28,9 @@ export class App extends Component {
     }));
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const { name, number } = this.state;
-    const constObj = {
-      id: nanoid(),
-      name: name,
-      number: number,
-    };
-
+  handleFormSubmit = data => {
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, constObj],
-      name: '',
-      number: '',
+      contacts: [...prevState.contacts, { ...data, id: nanoid() }],
     }));
   };
 
@@ -56,31 +46,7 @@ export class App extends Component {
 
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <ContactsForm
-            label="Name"
-            className="form-label"
-            name="name"
-            type="text"
-            placeholder="Ivan"
-            id={nanoid()}
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <ContactsForm
-            label="Number"
-            className="form-label"
-            name="number"
-            type="number"
-            placeholder="000-00-00"
-            id={nanoid()}
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-          <button className="addContactBtn" type="submit">
-            Add contact
-          </button>
-        </form>
+        <ContactsForm onSubmit={this.handleFormSubmit} />
         <Filter
           label="Name"
           name={this.state.name}
